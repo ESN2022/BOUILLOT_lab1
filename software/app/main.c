3ca94780b7fae5919b0e00582111fee3f0c8cd86
Clement BOUILLOT <clbouillot2>
2023-01-09 16:26:03 +0100

    gestion de la vitesse du chenillard par interruption

--- software/app/main.c
+++ software/app/main.c
@@ -1,16 +1,20 @@
 #include<system.h>
 #include<altera_avalon_pio_regs.h>
 #include <stdio.h>
+#include <sys/alt_irq.h>
 
 int i=0, time=0, sw;
 
-int main ()
+/*Définition de la routine d'interruption*/
+
+static void switch_interrupts ()
 {
-	//IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
-	while(1){
-		sw = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
-		switch (sw)
+	sw = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
+	switch (sw)
 		{
+		case 0 :
+			time = 10000;
+			break;
 		case 1 :
 			time = 100000;
 			break;
@@ -24,15 +28,35 @@ int main ()
 			time = 25000;
 			break;
 		}
-		for (i=0;i<7;i++){
-			   IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,1<<i);
-			   usleep(time);
-		}
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0xf);
+}
+
+
+int main ()
+{
+
+	/* Les 4 switchs vont pouvoir générer l'interruption */
+	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE, 0xf);
+
+	/*Initialisation du registre edge capture*/
+	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0xf);
+	
+	/* Definition du registre d'interruption */
+	alt_ic_isr_register(PIO_1_IRQ_INTERRUPT_CONTROLLER_ID,PIO_1_IRQ,(void*) switch_interrupts,NULL, 0x0);
+	
+	sw=0;
+	while(1){
 		
+		/*Boucle du chenillard*/
+		for (i=0;i<7;i++){
+			IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,1<<i);
+			usleep(time);
+		}
 		for (i=7;i>0;i--){
 			   IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,1<<i);
 			   usleep(time);
-			}
+			}	
 	}
 	return 0;
 }
+
