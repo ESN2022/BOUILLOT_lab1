5f3f9a859b15d03bb2f1bf57e49b490c939cde8e
Clement BOUILLOT <clbouillot2>
2023-01-09 14:16:02 +0100

    gestion de la vitesse par switch en pulling

--- software/app/main.c
+++ software/app/main.c
@@ -2,23 +2,25 @@
 #include<altera_avalon_pio_regs.h>
 #include <stdio.h>
 
-int i=0, time=0, sw=0;
+int i=0, time=0, sw;
 
 int main ()
 {
 	//IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 	while(1){
-		switch sw{
-		case 0 :
+		sw = IORD_ALTERA_AVALON_PIO_DATA(PIO_1_BASE);
+		switch (sw)
+		{
+		case 1 :
 			time = 100000;
 			break;
-		case 1 :
+		case 2 :
 			time = 75000;
 			break;
-		case 2 :
+		case 4 :
 			time = 50000;
 			break;
-		case 3 :
+		case 8 :
 			time = 25000;
 			break;
 		}
